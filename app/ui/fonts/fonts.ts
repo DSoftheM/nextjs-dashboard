c66e06b6b5d6f31baf989b55295dfc47231e5d71
artur <email@example.com>
2025-04-23 13:24:32 +0500

    wip

---
+++ app/ui/fonts/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from "next/font/google"
+import localFont from "next/font/local"
+
+export const inter = Inter({ subsets: ["latin"] })
+export const lusitana = Lusitana({ weight: ["400", "700"], subsets: ["latin"] })
+export const beautifulPoliceOfficer = localFont({ src: "./BeautifulPoliceOfficer.ttf" })
