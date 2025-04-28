abb5239d39b6ba0c2d36f22e9dc1bc0bd4344915
artur <email@example.com>
2025-04-28 14:08:59 +0500

    add skeletons

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,8 @@
-export default function Page() {
+async function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+export default async function Page() {
+  await delay(2000) // Simulate a delay of 2 seconds
   return <p>Customers Page</p>
 }
